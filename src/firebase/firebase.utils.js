30d05674e8aed417078b92a457009d5539203dc1
shankha001 <shankhasubhrabiswas@gmail.com>
2020-08-18 10:05:05 +0530

    [Firebase] firebase config

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
